1e77d6462a087d4712bcea6078d73df82a89f18d
Gabriel Neves <gabrielneves.dev@gmail.com>
2024-06-25 09:11:13 -0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './components/Home';
+
+
+import {register} from 'swiper/element/bundle';
+
+register()
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
