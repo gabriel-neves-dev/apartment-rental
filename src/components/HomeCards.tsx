55ccf9ab36aad5247c2afac1bcf7a2bdf7c7e561
Gabriel Neves <gabrielneves.dev@gmail.com>
2024-06-25 09:10:12 -0300

    added cards

---
+++ src/components/HomeCards.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Cards() {
+  return (
+    <div className="cards-container">
+      <div className="card">
+
+        <div className="placeholder">
+          <h1 className="div-text">Living Room</h1>
+        </div>
+
+      </div>
+      <div className="card1">
+
+        <div className="placeholder">
+          <h1 className="div-text">Bedroom</h1>
+        </div>
+
+      </div>
+      <div className="card2">
+
+        <div className="placeholder">
+          <h1 className="div-text">Kitchen</h1>
+        </div>
+
+        
+
+      </div>
+
+      
+    </div>
+  );
+}
+
+export default Cards;
